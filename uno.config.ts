ea36ddbd2ea49a1eafbcfc3f0e58165852e7f788
falentio <riv.kevinfalent.io@gmail.com>
2023-06-15 10:40:23 +0700

    add compile class

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,29 @@
 import { defineConfig, presetUno } from "unocss";
 import variantGroup from "@unocss/transformer-variant-group";
 import directives from "@unocss/transformer-directives";
+import compileClass from "@unocss/transformer-compile-class";
 import presetIcons from "@unocss/preset-icons";
 import extractorSvelte from "@unocss/extractor-svelte";
 
+const prod = process.env.NODE_ENV === "production"
+const trigger = /(?<=(class\=|\"class\"\,\s))\"([^']*?)"/gi
+// debugging purpose
+// const ril = String.prototype.matchAll
+// String.prototype.matchAll = function () {
+//     if (trigger === arguments[0])
+//         console.log([...ril.bind(this)(...arguments), 12345678][0])
+//     return ril.bind(this)(...arguments)
+// }
+
+const createHash = () => {
+    const classes = new Set()
+    return (s: string) => {
+        s = s.split(" ").sort().join(" ")
+        classes.add(s)
+        const index = [...classes].indexOf(s)
+        return index.toString(36)
+    }
+}
 export default defineConfig({
     presets: [
         presetUno(),
@@ -18,12 +38,20 @@ export default defineConfig({
             },
         }),
     ],
-    transformers: [variantGroup(), directives()],
+    transformers: [
+        prod && compileClass({
+            trigger,
+            hashFn: createHash(),
+            classPrefix: ""
+        }),
+        variantGroup(),
+        directives()
+    ].filter(Boolean) as any[],
     extractors: [extractorSvelte()],
     theme: {
         fontFamily: {
             comic: "'Comic Mono', sans-serif",
         },
     },
-    safelist: ["hidden"]
+    safelist: ["hidden"],
 });
