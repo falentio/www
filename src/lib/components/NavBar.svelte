da543a38b632277aeafd67887e15f696b57fc78b
falentio <riv.kevinfalent.io@gmail.com>
2023-06-15 11:34:16 +0700

    prevent scroll whenever navbar open

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
 	import { page } from "$app/stores";
+	const onChange = (e: { currentTarget: HTMLInputElement }) => {
+		const body = document.querySelector("body");
+		if (!body) {
+			return;
+		}
+		if (e.currentTarget.checked) {
+			body.style.overflow = "hidden";
+		} else {
+			body.style.overflow = "auto";
+		}
+	};
 </script>
 
 <div class="<sm:(bottom-0 fixed) sm:(sticky top-0) flex flex-col w-max z-30">
-	<input type="checkbox" id="menu" class="hidden peer" />
+	<input type="checkbox" id="menu" class="hidden peer" on:change={onChange} />
 	<div
 		class="flex-auto transition-all duration-350 pointer-events-auto -left-full <sm:(relative text-2xl peer-checked:left-0) flex flex-row"
 	>
-		<nav class="p-2 text-xl sm:(h-screen bg-zinc-8) overflow-y-auto flex flex-col">
+		<nav class="p-2 text-xl sm:(h-screen bg-zinc-8 w-36) overflow-y-auto flex flex-col">
 			{#if $page.url.pathname !== "/"}
 				<a href="/" class="<sm:hidden text-3xl inline-flex flex-col space-y-1 items-center">
 					<span class="w-16 h-16 block overflow-hidden rounded-full">
@@ -21,33 +32,32 @@
 				</a>
 				<span class="<sm:hidden bg-sky-2 min-h-[0.0125rem] block my-1 w-full" />
 			{/if}
-			<label for="menu" class="-mx-2 flex-auto w-screen sm:hidden">
-				<div class="h-screen" />
+			<label for="menu" class="-mx-2 flex-auto <sm:w-screen w-full block">
+				<div class="h-screen sm:hidden" />
 				<div
 					class="
-						text-sky-2 px-2 <sm:space-y-1 overflow-hidden flex flex-col w-max
-						[&>span]:(flex flex-row gap-2 items-center p-1)
-						[&>span]:<sm:(bg-zinc-8 rounded-lg px-3 text-sm)
-						[&>span>span]:(w-4 h-4)
-						[&>span>a]:(underline text-blue-5 capitalize)
+						text-sky-2 px-2 <sm:space-y-1 overflow-hidden flex flex-col w-max text-sky-4 capitalize underline
+						[&>a]:(flex flex-row gap-2 items-center p-1)
+						[&>a]:<sm:(bg-zinc-8 rounded-lg px-3 text-sm)
+						[&>a>span]:(w-4 h-4)
 					"
 				>
-					<span>
+					<a class="hover:text-sky-6" href="/">
 						<span class="i-mdi:home" />
-						<a href="/">Home</a>
-					</span>
-					<span>
+						Home
+					</a>
+					<a class="hover:text-sky-6" href="/">
 						<span class="i-mdi:post" />
-						<a href="/blog">blog</a>
-					</span>
-					<span>
+						blog
+					</a>
+					<a class="hover:text-sky-6" href="/">
 						<span class="i-mdi:library-books" />
-						<a href="/projects">Projects</a>
-					</span>
-					<span>
+						Projects
+					</a>
+					<a class="hover:text-sky-6" href="/">
 						<span class="i-mdi:information-outline" />
-						<a href="/about">About</a>
-					</span>
+						About
+					</a>
 				</div>
 			</label>
 		</nav>
