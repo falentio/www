f89b859f92628ef1e6c08d98551a21e9eb2c80ab
falentio <riv.kevinfalent.io@gmail.com>
2023-10-03 22:19:09 +0700

    fix large layout shifting

--- src/lib/components/Lang.svelte
+++ src/lib/components/Lang.svelte
@@ -5,9 +5,3 @@
 
 <span class="{logo} h-6 w-6 self-center text-blue-7 inline-block vertical-end mx-1" />
 <a {href} target="_blank" referrerpolicy="no-referrer" rel="external" class="underline"><slot /></a>
-
-<style>
-	div {
-		--uno: "inline-flex vertical-end gap-1 antialiased outline w-min";
-	}
-</style>
