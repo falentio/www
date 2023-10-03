9109dd7454c04d4e02e124df38387ec4c791b094
falentio <riv.kevinfalent.io@gmail.com>
2023-10-03 22:34:31 +0700

    fix large layout shifting

--- src/lib/components/Lang.svelte
+++ src/lib/components/Lang.svelte
@@ -3,5 +3,5 @@
 	export let href: string;
 </script>
 
-<span class=" h-6 w-6 self-center text-blue-7 inline-block vertical-end mx-1" />
+<span class="{logo} h-6 w-6 text-blue-7 inline-block vertical-end mx-1" />
 <a {href} target="_blank" referrerpolicy="no-referrer" rel="external" class="underline"><slot /></a>
