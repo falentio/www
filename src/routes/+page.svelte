24f6fd45874eb89049dc953a397fbf8f210fa7d1
falentio <riv.kevinfalent.io@gmail.com>
2023-10-03 22:29:42 +0700

    fix large layout shifting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 <div class="flex flex-row <md:(flex-col text-center p-4) px-4 py-16 gap-8 justify-between">
 	<section class="flex flex-col gap-4">
 		<h1 class="font-bold text-5xl text-blue-7">Kevin Falentio</h1>
-		<!-- <p class="items-center z-0">
+		<p>
 			Hello, I am Kevin Falentio and I am Software Developer. I mainly use<Lang
 				href="https://typescriptlang.org"
 				logo="i-mdi:language-typescript">Typescript</Lang
@@ -26,7 +26,7 @@
 			</Lang>,
 			<Lang href="https://en.wikipedia.org/wiki/HTML" logo="i-mdi:language-html5">HTML5</Lang>, and
 			<Lang href="https://en.wikipedia.org/wiki/CSS" logo="i-mdi:language-css3">CSS3</Lang>.
-		</p> -->
+		</p>
 	</section>
 	<Profile imgClass="w-48" class="<md:order-first" />
 </div>
